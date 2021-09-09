6ac7e0a98c3bf8d2e6b7597a0a7e07b50749f2f8
thomasplantin <>
2021-09-09 15:30:10 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # covid-data-visualization
+
+## Setting up the application
+
+- Ensure that you have the `virtualenv` package installed. If not run `pip3 install virtualenv`.
+
+- Create the virtual environment --> `virtualenv env`
+
+- Activate the virtual environment --> `source env/bin/activate`
+
+- Install all packages in your virtual environment --> `pip3 install -r requirements.txt`
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/cases')
+def cases():
+    return render_template("cases.html")
+
+@app.route('/deaths')
+def deaths():
+    return render_template("deaths.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
